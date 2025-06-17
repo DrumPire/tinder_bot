37ee680a21915cdc34402dca2108a5c40f31eb5a
DrumPire <expresproject@gmail.com>
2025-06-17 12:12:52 +0300

    add connect tg-bot for chat-gpt

--- bot.py
+++ bot.py
@@ -8,31 +8,44 @@ TOKEN = '7512240165:AAFNxz62TMDXdn9_Ggj9c0pzQ-IwxSG_RF8'
 # тут будемо писати наш код :)
 
 async def start(update, context):
-
-  # await send_photo(update, context, "avatar_main")
-  # await send_text(update, context, "Hello User")
+  await send_photo(update, context, 'main')
   msg = load_message('main')
-  await send_photo(update, context, 'avatar_main')
   await send_text(update, context, msg)
-
-
-async def hello(update, context):
-  # await send_text(update, context, "Hello" + update.message.text)
-  await send_text_buttons(update, context, "Hello" + ' ' + update.message.from_user.first_name + ' ' + update.message.from_user.last_name, {
-    'start': 'START',
-    'stop': 'STOP' 
+  await show_main_menu(update, context, {
+    'start':  'головне меню бота',
+    'profile':  'генерація Tinder-профілю 😎',
+    'opener':  'повідомлення для знайомства 🥰',
+    'message':  'листування від вашого імені 😈',
+    'date':  'листування із зірками 🔥',
+    'gpt':  'поставити запитання чату GPT 🧠'
   })
 
-async def buttons_nandler(update, context):
+async def gpt(update, context):
+  dialog.mode = 'gpt'
+  await send_photo(update, context, 'gpt')
+  msg = load_message('gpt')
+  await send_text(update, context, msg)
 
-  query = update.callback_query.data
-  if query == 'start':
-    await send_text(update, context, 'Started')
-  elif query == 'stop':
-    await send_text(update, context, 'Stoped')
+async def gpt_dialog(update, context):
+  text = update.message.text
+  prompt = load_prompt('gpt')
+  answer = await chatgpt.send_question(prompt, text) 
+  await send_text(update, context, answer)
+
+async def hello(update, context):
+  if dialog.mode == 'gpt':
+    await gpt_dialog(update, context)
+
+dialog = Dialog()
+dialog.mode = None
+
+chatgpt = ChatGptService(token='gpt:AU54YW8RRi4TXANWp060hfjiJxU6btLIvPmqxAgYF0QLgPDwmNfdLT5NyC9Y8r_u4QZeQmwhzFJFkblB3T4yhgCdA9W2KZIQwDchnwN-SRJKHph3pqraKQNsAmcDeSXdm_4aNY-8_3oiLFalGXckzNJlfA-T')
+
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler('start', start))
-app.add_handler(CallbackQueryHandler(buttons_nandler))
+app.add_handler(CommandHandler('gpt', gpt))
+# app.add_handler(CallbackQueryHandler(buttons_nandler))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.run_polling()
